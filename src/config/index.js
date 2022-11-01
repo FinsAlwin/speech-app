488ffeee9b026b33cfe064a47267191b16211f06
FinsAlwin <alwinwk101@live.com>
2022-11-01 14:40:09 +0530

    commit 13

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://main.d2avmmnv8axc1e.amplifyapp.com";
 
-export const SOCKET_URL = "https://main.d2j8ti8120xv20.amplifyapp.com/";
+export const SOCKET_URL = "https://main.d1rgr15fks5qdl.amplifyapp.com/";
