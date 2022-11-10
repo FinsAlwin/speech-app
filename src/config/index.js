d5c3ea189bfe782b38bd4882e6ac6c0bc17a8c41
FinsAlwin <alwinwk101@live.com>
2022-11-10 12:33:42 +0530

    18

--- src/config/index.js
+++ src/config/index.js
@@ -6,5 +6,5 @@ export const SOCKET_URL =
   "ws://ec2-3-108-64-255.ap-south-1.compute.amazonaws.com:8080/ws";
 
 export const socket = new WebSocket(
-  "wss://ec2-3-108-64-255.ap-south-1.compute.amazonaws.com:8080/ws"
+  "ws://ec2-3-108-64-255.ap-south-1.compute.amazonaws.com:8080/ws"
 );
