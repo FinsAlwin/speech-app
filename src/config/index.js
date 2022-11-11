f671c9fe1faa1962707d52253f4b89a148990007
FinsAlwin <alwinwk101@live.com>
2022-11-11 10:06:43 +0530

    21

--- src/config/index.js
+++ src/config/index.js
@@ -6,5 +6,5 @@ export const SOCKET_URL =
   "ws://ec2-3-108-64-255.ap-south-1.compute.amazonaws.com:8080/ws";
 
 export const socket = new WebSocket(
-  "https://ec2-3-108-64-255.ap-south-1.compute.amazonaws.com:8080/ws"
+  "wss://gosocket.herokuapp.com/ws"
 );
