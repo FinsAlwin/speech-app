ba12ebf7c04e10ff126112085b4407a0a4ca5f43
FinsAlwin <alwinwk101@live.com>
2022-11-11 10:14:19 +0530

    22

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,8 @@
-export const BASE_URL = window.location;
+export const BASE_URL = "https://main.druq7cgf4v9f.amplifyapp.com";
 
 // export const SOCKET_URL = "ws://localhost:8080/ws";
 
 export const SOCKET_URL =
   "ws://ec2-3-108-64-255.ap-south-1.compute.amazonaws.com:8080/ws";
 
-export const socket = new WebSocket(
-  "wss://gosocket.herokuapp.com/ws"
-);
+export const socket = new WebSocket("wss://gosocket.herokuapp.com/ws");
